8cce5d866c2bb3298b04ac50290b1e6e4fde7c3e
Taaliah Ayub <taaliah2003@gmail.com>
2025-01-11 16:45:05 -0500

    databases models + admin reg

--- project/test/admin.py
+++ project/test/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Animal_Log, Animals
 
 # Register your models here.
+@admin.register(Animal_Log)
+class AnimalLogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'loc')
+
+@admin.register(Animals)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- project/test/models.py
+++ project/test/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Animal_Log(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    loc = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Animals(models.Model):
+    name = models.CharField(max_length=100)
+    # photo = models.ImageField(upload_to='/photos')
+
+    def __str__(self):
+        return self.name
