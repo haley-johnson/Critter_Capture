6139fa0e8b7306770f9000fdeb174765845c32a4
alyssawang3 <wanga121@mcmaster.ca>
2025-01-11 19:29:43 -0500

    updated view page

--- project/test/views.py
+++ project/test/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Animal_Log, Animals
+import requests
+
+API_KEY = 'VSrG5uDX/jdGVlbkMmLrDA==pW0aLq36IRlIssCj' 
 
 # View for the home page
 def home(request):
@@ -7,11 +10,31 @@ def home(request):
 
 # View for the contact page
 def critters(request):
-    return render(request, 'test/critters.html')
+    print("HERE")
+    logs = Animal_Log.objects.all()  # Fetch all entries from the database
+    print(logs)
+    logs_with_api_data = []
+
+    # for log in logs:
+    #     response = requests.get(
+    #         'https://api.api-ninjas.com/v1/animals',
+    #         headers={'X-Api-Key': API_KEY},
+    #         params={'name': log.name}
+    #     )
+
+    #     if response.status_code == 200:
+    #         api_data = response.json()
+    #     else:
+    #         api_data = {"error": "Could not fetch data"}
+
+    #      # Add log and API data to the list
+    #     logs_with_api_data.append({
+    #         "log": log,
+    #         "api_data": api_data
+    #     })
+    # print(logs_with_api_data)
+    
+    return render(request, 'test/critters.html', {'logs': logs})
 
 def conservation(request):
     return render(request, 'test/conservation.html')
-
-def critter_collection(request):
-    logs = Animal_Log.objects.all()  # Fetch all entries from the database
-    return render(request, 'critters.html', {'logs': logs})
